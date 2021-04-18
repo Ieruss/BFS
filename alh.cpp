658c175da42bebad269be53e439be064059790e9
Ieruss <82746962+Ieruss@users.noreply.github.com>
2021-04-18 12:22:17 +0600

    Update alh.cpp

--- alh.cpp
+++ alh.cpp
@@ -3,11 +3,11 @@ inline void bfs(vector<int> &d, int x){
 	queue<pair<int,int> >q;
 	q.push({x,d[x]});
 	while(!q.empty()){
-		auto it = q.front();
+		pair<int,int> it = q.front();
 		q.pop();
-		for(auto i : g[it.F]){
-			if(d[i] > it.S + 1){
-				d[i] = it.S + 1;
+		for(int i : g[it.first]){
+			if(d[i] > it.second + 1){
+				d[i] = it.second + 1;
 				q.push({i,d[i]});
 			}
 		}
