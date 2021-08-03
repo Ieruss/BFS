491c36976b08f83a44dd11dae0df231e039c032b
Ieruss <82746962+Ieruss@users.noreply.github.com>
2021-08-03 10:25:46 +0600

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,12 +1,12 @@
-void bfs(vector<int> &d, int x){
+inline void bfs(vector<int> &d, int x){
 	d[x] = 0;
-	queue<pair<int,int> >q;
+	queue< pair<int , int> >q;
 	q.push({x,d[x]});
 	while(!q.empty()){
 		pair<int,int> it = q.front();
 		q.pop();
 		for(int i : g[it.first]){
-			if(d[i] > it.second + 1){
+			if(d[i] == ){
 				d[i] = it.second + 1;
 				q.push({i,d[i]});
 			}
